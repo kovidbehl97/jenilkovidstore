a8d64499620e564e7131bad6b9c59c849b701707
kovidbehl97 <kovid.behl@gmail.com>
2024-07-23 09:31:10 -0400

    new

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -12,7 +12,7 @@ export default function PaymentPage() {
 			{JSON.stringify(session)}
 			<div>
 				<button
-					className="mt-4 px-4 py-2 border-2"
+					className="mt-4 px-4 py-2 border-2 "
 					onClick={() => signOut()}
 				>
 					logout
